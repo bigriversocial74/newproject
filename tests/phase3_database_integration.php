aa076b7e6f1798691447cd73ed2b89daa8ed8109
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-28 21:19:21 -0700

    Fix Phase 3 integration test database bootstrap

--- tests/phase3_database_integration.php
+++ tests/phase3_database_integration.php
@@ -8,7 +8,23 @@ use Throwable;
 use Vp3\Database;
 use Vp3\Licensing\DomainLicenseBundleService;
 
-require dirname(__DIR__) . '/bootstrap.php';
+$projectRoot = dirname(__DIR__);
+$autoload = $projectRoot . '/vendor/autoload.php';
+if (is_file($autoload)) {
+    require $autoload;
+} else {
+    spl_autoload_register(static function (string $class) use ($projectRoot): void {
+        $prefix = 'Vp3\\';
+        if (!str_starts_with($class, $prefix)) {
+            return;
+        }
+
+        $path = $projectRoot . '/src/' . str_replace('\\', '/', substr($class, strlen($prefix))) . '.php';
+        if (is_file($path)) {
+            require $path;
+        }
+    });
+}
 
 $dsn = getenv('VP3_TEST_DSN') ?: '';
 $username = getenv('VP3_TEST_DB_USER') ?: 'root';
