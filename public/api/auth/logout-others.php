2bc76b1e58939b36032d73e1fd881f8b43d590f6
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-29 12:26:03 -0700

    Add logout-other-sessions endpoint

---
+++ public/api/auth/logout-others.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Vp3\Http\AuthEndpoint;
+use Vp3\Http\JsonResponse;
+
+$container = require dirname(__DIR__, 3) . '/bootstrap.php';
+AuthEndpoint::requireMethod('POST');
+
+try {
+    $payload = AuthEndpoint::payload();
+    $container['session']->assertCsrf(AuthEndpoint::csrf($payload));
+    $current = $container['authentication_context']->requireCurrent(AuthEndpoint::ip(), AuthEndpoint::userAgent(), false);
+    $revoked = $container['database_sessions']->revokeAllForUser(
+        $current['user']['id'],
+        'logout_others',
+        $current['session']['public_id']
+    );
+    JsonResponse::send(['data' => ['status' => 'other_sessions_revoked', 'revoked_sessions' => $revoked]]);
+} catch (Throwable $exception) {
+    AuthEndpoint::sendException($exception);
+}
