f4fec528de90fc59de9fc98cca9792d5d5bf527d
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-28 20:16:37 -0700

    Add cumulative VP3 single-install SQL entry point

---
+++ database/vp3-single-install.sql
@@ -0,0 +1,4 @@
+SOURCE migrations/20260728_phase2_auth_accounts.sql;
+SOURCE migrations/20260729_phase2_auth_accounts_quality.sql;
+SOURCE migrations/20260729_phase2_auth_accounts_completion.sql;
+SOURCE migrations/20260729_phase3_domain_plans_licenses.sql;
