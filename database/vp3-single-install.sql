d45ad910d01edbc4788d89bdfb683c603cbc8a2b
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-29 15:31:09 -0700

    Include HomeServer control-plane migration

--- database/vp3-single-install.sql
+++ database/vp3-single-install.sql
@@ -16,3 +16,4 @@ SOURCE migrations/20260729_phase10_operations_readiness.sql;
 SOURCE migrations/20260729_phase11a_runtime_queue_hardening.sql;
 SOURCE migrations/20260729_phase11b_identity_authentication_completion.sql;
 SOURCE migrations/20260729_phase11b_identity_authentication_integrity.sql;
+SOURCE migrations/20260729_phase12_homeserver_control_plane.sql;
