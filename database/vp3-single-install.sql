1529e521d05bf3da357cd4a411cb3717a45b0720
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-29 01:23:48 -0700

    Include target-scoped snapshot identity migration

--- database/vp3-single-install.sql
+++ database/vp3-single-install.sql
@@ -9,3 +9,4 @@ SOURCE migrations/20260729_phase7_releases_updates.sql;
 SOURCE migrations/20260729_phase7_release_manifest_documents.sql;
 SOURCE migrations/20260729_phase8_backups_storage.sql;
 SOURCE migrations/20260729_phase8_backup_job_snapshots.sql;
+SOURCE migrations/20260729_phase8_snapshot_identity.sql;
