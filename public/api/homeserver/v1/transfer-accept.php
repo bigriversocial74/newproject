1058f0707100f0c288f3e84a30bfcb43015ebf18
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-29 15:30:23 -0700

    Add HomeServer transfer acceptance endpoint

---
+++ public/api/homeserver/v1/transfer-accept.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Vp3\Http\HomeServerEndpoint;
+use Vp3\Http\JsonResponse;
+
+$container = require __DIR__ . '/_bootstrap.php';
+HomeServerEndpoint::requireMethod('POST');
+try {
+    $payload = HomeServerEndpoint::payload();
+    $account = HomeServerEndpoint::accountContext($container, $payload);
+    $result = $container['homeserver_control_plane']->acceptTransfer(
+        $account['account_id'],
+        (string) ($payload['transfer_code'] ?? ''),
+        (int) ($payload['target_license_id'] ?? 0),
+        HomeServerEndpoint::requestId($payload)
+    );
+    JsonResponse::send(['data' => $result]);
+} catch (Throwable $exception) {
+    HomeServerEndpoint::sendException($exception);
+}
