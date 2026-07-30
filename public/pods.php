4e4b0abc77db030de4e9c6c73820d911e872fe14
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-30 00:20:30 -0700

    Render safe POD access failures

--- public/pods.php
+++ public/pods.php
@@ -9,8 +9,8 @@ use Vp3\ControlCenter\ControlCenterPage;
 $container = require dirname(__DIR__) . '/bootstrap.php';
 try {
     $context = AccountPageContext::resolve($container);
-} catch (AuthPublicException) {
-    ControlCenterPage::renderSignInRequired('VP3 PODs');
+} catch (AuthPublicException $exception) {
+    ControlCenterPage::renderAccessFailure('VP3 PODs', $exception);
 }
 ControlCenterPage::renderStart(
     $context,
