5ed3f09c5b85fc07a299d34ead064f428f9bee37
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-30 00:18:33 -0700

    Sign VP3 federated settings snapshots

--- public/api/settings/v1/snapshot.php
+++ public/api/settings/v1/snapshot.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Vp3\Http\HomeServerEndpoint;
 use Vp3\Http\JsonResponse;
 use Vp3\Settings\FederatedSettingsService;
+use Vp3\Settings\FederatedSettingsSigner;
 
 $container = require dirname(__DIR__, 4) . '/bootstrap.php';
 HomeServerEndpoint::requireMethod('POST');
@@ -13,10 +14,11 @@ try {
     $payload = HomeServerEndpoint::payload();
     $account = HomeServerEndpoint::accountContext($container, $payload);
     $service = new FederatedSettingsService($container['database']);
-    JsonResponse::send(['data' => $service->snapshotForAccount(
+    $signer = new FederatedSettingsSigner($container['homeserver_lease_signer']);
+    JsonResponse::send(['data' => $signer->sign($service->snapshotForAccount(
         $account['account_id'],
         isset($payload['device_public_id']) ? (string) $payload['device_public_id'] : null
-    )]);
+    ))]);
 } catch (Throwable $exception) {
     HomeServerEndpoint::sendException($exception);
 }
