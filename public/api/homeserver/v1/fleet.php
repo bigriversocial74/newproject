390441cdfc6a24172035e3758e22f210c2a42315
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-29 21:39:52 -0700

    Expose authenticated HomeServer fleet snapshot

---
+++ public/api/homeserver/v1/fleet.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Vp3\HomeServers\HomeServerFleetQueryService;
+use Vp3\Http\HomeServerEndpoint;
+use Vp3\Http\JsonResponse;
+
+$container = require __DIR__ . '/_bootstrap.php';
+HomeServerEndpoint::requireMethod('POST');
+
+try {
+    $payload = HomeServerEndpoint::payload();
+    $account = HomeServerEndpoint::accountContext($container, $payload);
+    $fleet = new HomeServerFleetQueryService($container['database']);
+    JsonResponse::send(['data' => $fleet->snapshot($account['account_id'])]);
+} catch (Throwable $exception) {
+    HomeServerEndpoint::sendException($exception);
+}
