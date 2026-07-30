2f42ad5b5269a6ed633945fccee2e198adf72dc4
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-30 10:06:56 -0700

    Add Phase 19 recovery overview API

---
+++ public/api/control-center/v1/recovery-overview.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Vp3\Http\ControlCenterEndpoint;
+use Vp3\Http\JsonResponse;
+use Vp3\Recovery\RecoveryControlCenterQueryService;
+
+$container = require __DIR__ . '/_bootstrap.php';
+ControlCenterEndpoint::requireMethod('POST');
+try {
+    $payload = ControlCenterEndpoint::payload();
+    $account = ControlCenterEndpoint::accountContextForRoles($container, $payload, ['customer_owner', 'customer_admin']);
+    $query = new RecoveryControlCenterQueryService($container['database']);
+    JsonResponse::send(['data' => $query->snapshot($account['account_id'])]);
+} catch (Throwable $exception) {
+    ControlCenterEndpoint::sendException($exception);
+}
