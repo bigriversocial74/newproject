cf1b6dcd914b6f1388d2dc344117ecbd531e06dd
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-30 19:40:40 -0700

    Configure HomeServer browser request integrity

--- public/api/homeserver/v1/_bootstrap.php
+++ public/api/homeserver/v1/_bootstrap.php
@@ -3,8 +3,15 @@
 declare(strict_types=1);
 
 use Vp3\HomeServers\HomeServerControlPlaneService;
+use Vp3\Http\BrowserRequestIntegrity;
+use Vp3\Http\HomeServerEndpoint;
 
 $container = require dirname(__DIR__, 4) . '/bootstrap.php';
+$requestIntegrity = new BrowserRequestIntegrity(
+    (string) $container['config']['app']['base_url'],
+    (string) $container['config']['app']['env']
+);
+HomeServerEndpoint::configureRequestIntegrity($requestIntegrity);
 $container['homeserver_control_plane'] = new HomeServerControlPlaneService(
     $container['database'],
     $container['homeservers'],
