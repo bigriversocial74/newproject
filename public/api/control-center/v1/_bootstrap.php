8f82561f7904723d444c273ab589c7d30bcfe537
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-30 19:39:36 -0700

    Configure Control Center browser request integrity

--- public/api/control-center/v1/_bootstrap.php
+++ public/api/control-center/v1/_bootstrap.php
@@ -2,9 +2,16 @@
 
 declare(strict_types=1);
 
+use Vp3\Http\BrowserRequestIntegrity;
+use Vp3\Http\ControlCenterEndpoint;
 use Vp3\Http\PublicResponseGuard;
 
 $container = require dirname(__DIR__, 4) . '/bootstrap.php';
+$requestIntegrity = new BrowserRequestIntegrity(
+    (string) $container['config']['app']['base_url'],
+    (string) $container['config']['app']['env']
+);
+ControlCenterEndpoint::configureRequestIntegrity($requestIntegrity);
 PublicResponseGuard::enable();
 
 return $container;
